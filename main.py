6798eae34dbe868df01c3e6af8e66fe7622aae84
Vivid-Clarity <thisumtheumin@gmail.com>
2025-07-24 01:01:21 +1000

    Adding ablity to add custom categories, diffrent from banks catagories so I retaining existing data

--- main.py
+++ main.py
@@ -6,6 +6,24 @@ import os
 
 st.set_page_config(page_title="Mah Finances", layout="wide")
 
+custom_catagory_file = "custom_categories.json"
+
+#Store categories in session state
+if "custom_catagory" not in st.session_state:
+    st.session_state.categories = {
+        "Uncategorized": []
+    }
+
+#Load categories from JSON file if it exists
+if os.path.exists(custom_catagory_file):
+    with open(custom_catagory_file, "r") as f:
+        st.session_state.categories = json.load(f)
+
+def save_categories():
+    """Save categories to JSON file"""
+    with open(custom_catagory_file, "w") as f:
+        json.dump(st.session_state.categories, f, indent=4)
+
 def load_transactions(file):
     """Use pandas to process the csv data from uploaded file
 
@@ -18,7 +36,7 @@ def load_transactions(file):
     try:
         #try to read the csv file
         df = pd.read_csv(file)
-        df.columns = [col.strip() for col in df.columns]  # Strip whitespace from column names
+        df.columns = [col.strip() for col in df.columns]  #Strip whitespace from column names
         df["Date"] = pd.to_datetime(df["Date"], format="%d %b %y")  #Turn date into pandsa acceptable format
         df["Amount"] = df["Amount"].astype(float)  #Ensure Amount is float
         df["Balance"] = df["Balance"].astype(float) #Ensure Balance is float
@@ -51,7 +69,17 @@ def main():
 
             tab1, tab2 = st.tabs(["Expenses (In/Debit)", "Payments (Out/Credit)"])
             with tab1:
+                new_catagory = st.text_input("New Custom Category name")
+                add_button = st.button("Add Custom Category")
+
+                if add_button and new_catagory:
+                    if new_catagory not in st.session_state.categories:
+                        st.session_state.categories[new_catagory] = []
+                        save_categories()
+                        st.rerun()
+
                 st.write(in_df)
+
             with tab2:
                 st.write(out_df)
             
